df0956e6406120cfb23a4c411fe49a4dfba08d1e
huy.tran <tranvanhuyy13@gmail.com>
2025-11-05 10:47:28 +0700

    remove venv

--- FullStack_Ecommerce_App/backend/product/views.py
+++ FullStack_Ecommerce_App/backend/product/views.py
@@ -1,99 +1,85 @@
-from .models import Product, Rating, Report
-from rest_framework import status, viewsets, permissions
-from rest_framework.exceptions import ValidationError
-from django.shortcuts import get_object_or_404
-from .serializers import ProductSerializer, RatingSerializer, ReportSerializer
+from .models import Product
+from rest_framework import status
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import ProductSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import action
-from django.db.models import Avg
+from rest_framework import authentication, permissions
+from rest_framework.decorators import permission_classes
 
 
-class ProductViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet for managing products.
-    """
+class ProductView(APIView):
 
-    serializer_class = ProductSerializer
-    queryset = Product.objects.all()
-
-    def get_permissions(self):
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            permission_classes = [permissions.IsAdminUser]
-        else:
-            permission_classes = [permissions.AllowAny]
-        return [permission() for permission in permission_classes]
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class RatingViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet for managing product ratings.
-    """
+class ProductDetailView(APIView):
 
-    serializer_class = RatingSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def get_queryset(self):
-        product_id = self.kwargs.get("product_pk")
-        if product_id:
-            return Rating.objects.filter(product_id=product_id)
-        return Rating.objects.filter(user=self.request.user)
 
-    def perform_create(self, serializer):
-        product_id = self.kwargs.get("product_pk")
-        product = get_object_or_404(Product, id=product_id)
+class ProductCreateView(APIView):
 
-        # Check if user already rated this product
-        if Rating.objects.filter(user=self.request.user, product=product).exists():
-            raise ValidationError("You have already rated this product")
+    permission_classes = [permissions.IsAdminUser]
 
-        serializer.save(user=self.request.user, product=product)
+    def post(self, request):
+        user = request.user
+        data = request.data
 
-    @action(detail=True, methods=["post"])
-    def update_rating(self, request, pk=None):
-        rating = self.get_object()
-        serializer = self.get_serializer(rating, data=request.data, partial=True)
+        product = {
+            "name": data["name"],
+            "description": data["description"],
+            "price": data["price"],
+            "stock": data["stock"],
+            "image": data["image"],
+        }
 
+        serializer = ProductSerializer(data=product, many=False)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ReportViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet for managing product and seller reports.
-    """
-
-    serializer_class = ReportSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        if not self.request.user.is_staff:
-            return Report.objects.filter(user=self.request.user)
-        return Report.objects.all()
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-    def get_permissions(self):
-        if self.action in ["update", "partial_update", "change_status"]:
-            permission_classes = [permissions.IsAdminUser]
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            permission_classes = [permissions.IsAuthenticated]
-        return [permission() for permission in permission_classes]
+            return Response({"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=True, methods=["post"])
-    def change_status(self, request, pk=None):
-        report = self.get_object()
-        if not request.user.is_staff:
-            return Response(
-                {"detail": "Only staff can update report status"},
-                status=status.HTTP_403_FORBIDDEN,
-            )
 
-        serializer = self.get_serializer(report, data=request.data, partial=True)
+class ProductDeleteView(APIView):
 
+    permission_classes = [permissions.IsAdminUser]
+
+    def delete(self, request, pk):
+        try:
+            product = Product.objects.get(id=pk)
+            product.delete()
+            return Response({"detail": "Product successfully deleted."}, status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+
+class ProductEditView(APIView):
+    
+    permission_classes = [permissions.IsAdminUser]
+
+    def put(self, request, pk):
+        data = request.data
+        product = Product.objects.get(id=pk)
+        
+        updated_product = {
+            "name": data["name"] if data["name"] else product.name,
+            "description": data["description"] if data["description"] else product.description,
+            "price": data["price"] if data["price"] else product.price,
+            "stock": data["stock"],
+            "image": data["image"] if data["image"] else product.image,
+        }
+
+        serializer = ProductSerializer(product, data=updated_product)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
