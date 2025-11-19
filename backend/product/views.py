aafcbf456df12c254abf6cebd0626db4851aaadc
huy.tran <tranvanhuyy13@gmail.com>
2025-11-20 00:40:29 +0700

    feature product view

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,82 +1,24 @@
-from .models import StripeModel, BillingAddress, OrderModel
-from django.http import Http404
+from .models import Product
 from rest_framework import status
+from django.shortcuts import render
 from rest_framework.views import APIView
-from django.contrib.auth.models import User
+from .serializers import ProductSerializer
 from rest_framework.response import Response
-from django.contrib.auth.hashers import make_password
 from rest_framework import authentication, permissions
 from rest_framework.decorators import permission_classes
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer 
-from rest_framework_simplejwt.views import TokenObtainPairView # for login page
-from django.contrib.auth.hashers import check_password
-from django.shortcuts import get_object_or_404
-from .serializers import (
-    UserSerializer, 
-    UserRegisterTokenSerializer, 
-    CardsListSerializer, 
-    BillingAddressSerializer,
-    AllOrdersListSerializer
-)
 
 
-# register user
-class UserRegisterView(APIView):
-    """To Register the User"""
-
-    def post(self, request, format=None):
-        data = request.data # holds username and password (in dictionary)
-        username = data["username"]
-        email = data["email"]
-
-        if username == "" or email == "":
-            return Response({"detial": "username or email cannot be empty"}, status=status.HTTP_400_BAD_REQUEST)
-
-        else:
-            check_username = User.objects.filter(username=username).count()
-            check_email =  User.objects.filter(email=email).count()
-
-            if check_username:
-                message = "A user with that username already exist!"
-                return Response({"detail": message}, status=status.HTTP_403_FORBIDDEN)
-            if check_email:
-                message = "A user with that email address already exist!"
-                return Response({"detail": message}, status=status.HTTP_403_FORBIDDEN)
-            else:
-                user = User.objects.create(
-                    username=username,
-                    email=email,
-                    password=make_password(data["password"]),
-                )
-                serializer = UserRegisterTokenSerializer(user, many=False)
-                return Response(serializer.data)
-
-# login user (customizing it so that we can see fields like username, email etc as a response 
-# from server, otherwise it will only provide access and refresh token)
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    
-    def validate(self, attrs):
-        data = super().validate(attrs)
-
-        serializer = UserRegisterTokenSerializer(self.user).data
-
-        for k, v in serializer.items():
-            data[k] = v
-        
-        return data
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
-
-
-# list all the cards (of currently logged in user only)
-class CardsListView(APIView):
-
-    permission_classes = [permissions.IsAuthenticated]
+class ProductView(APIView):
 
     def get(self, request):
-        # show stripe cards of only that user which is equivalent 
-        #to currently logged in user
-        stripeCards = StripeModel.objects.filter(user=request.user)
-        serializer = CardsListSerializer(stripeCards, many=True)
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProductDetailView(APIView):
+
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product, many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
