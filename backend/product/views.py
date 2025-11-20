8f8f5469a51c13aa893704c6ae3ff22c3a2ac5d5
anhtuantran0413 <anhtuantran0413@gmail.com>
2025-11-20 11:43:05 +0700

    feature create product view

--- backend/product/views.py
+++ backend/product/views.py
@@ -22,3 +22,26 @@ class ProductDetailView(APIView):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(product, many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class ProductCreateView(APIView):
+
+    permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request):
+        user = request.user
+        data = request.data
+
+        product = {
+            "name": data["name"],
+            "description": data["description"],
+            "price": data["price"],
+            "stock": data["stock"],
+            "image": data["image"],
+        }
+
+        serializer = ProductSerializer(data=product, many=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
